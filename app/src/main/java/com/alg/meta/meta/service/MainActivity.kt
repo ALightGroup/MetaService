3174b63822b67d8e3bee4303afc8c9c21563a39a
laoyuyu <laoyuyu@tinman.cn>
2022-12-07 10:39:48 +0800

    服务

---
+++ app/src/main/java/com/alg/meta/meta/service/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alg.meta.meta.service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
